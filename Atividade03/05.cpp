8f273d86365746389d3bf62cd76fc68c16530995
davigomesflorencio <davigomesflorencio@gmail.com>
2021-06-10 22:26:11 -0300

    Modificaçao na Questao 05 da Atividade03

--- Atividade03/05.cpp
+++ Atividade03/05.cpp
@@ -1,42 +1,41 @@
-#include <iostream>
 #include <bits/stdc++.h>
+#include <iostream>
 #include <stack>
 
 using namespace std;
 
 /**
- * 
+ *
  * 921. Minimum Add to Make Parentheses Valid
- * 
+ *
  **/
 
 class Solution {
 public:
-    int minAddToMakeValid(string s) {
-        int res=0;
-        stack<int> st;
-        for(auto x:s){ 
-            if(st.empty()){
-                st.push(x);
-            }else{
-                if(x==')' && st.top()=='('){
-                    st.pop();
-                }else{
-                    st.push(x);
-                }
-            }
+  int minAddToMakeValid(string s) {
+    int res = 0;
+    stack<int> st;
+    for (auto x : s) {
+      if (st.empty()) {
+        st.push(x);
+
+      } else {
+        if (x == ')' && st.top() == '(') {
+          st.pop();
+        } else {
+          st.push(x);
         }
-        return st.size();
+      }
     }
+    return st.size();
+  }
 };
 
-int main(int argc, char const *argv[]){
-    Solution so;
-    string v="))((";
-    int ola = so.minAddToMakeValid(v);
-
-    cout << ola << " ";
-
-    cout<<' '<<endl; /* code */
-    return 0;
+int main(int argc, char const *argv[]) {
+  Solution so;
+  string v = "))((";
+  int ola = so.minAddToMakeValid(v);
+  cout << ola << " ";
+  cout << ' ' << endl;
+  return 0;
 }
